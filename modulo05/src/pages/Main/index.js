cc41fb626a7c764791f268617b3839b4e52d4769
LeandroR94 <reisleandro868@gmail.com>
2019-09-22 13:40:50 -0300

    Estilizando página Main

--- modulo05/src/pages/Main/index.js
+++ modulo05/src/pages/Main/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
-import { Title } from './styles';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import { Container, Form, SubmitButton } from './styles';
 
-function Main() {
-  return <Title>Olá mundo</Title>;
+export default function Main() {
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar repositório" />
+        <SubmitButton type="submit">
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
-export default Main;
