57f884848f09198b014e4af24d9db81920f15fd9
LeandroR94 <reisleandro868@gmail.com>
2019-09-22 17:25:52 -0300

    Navegação de rotas

--- modulo05/src/routes.js
+++ modulo05/src/routes.js
@@ -9,7 +9,7 @@ export default function Routes() {
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Main} />
-        <Route path="/respository" component={Repository} />
+        <Route path="/respository/:repository" component={Repository} />
       </Switch>
     </BrowserRouter>
   );
