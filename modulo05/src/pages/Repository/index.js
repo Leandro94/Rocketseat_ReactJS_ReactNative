29e4ac0ada43387901772d3f58a80ca7fce02bbf
LeandroR94 <reisleandro868@gmail.com>
2019-09-28 18:17:42 -0300

    Exibindo repositório

--- modulo05/src/pages/Repository/index.js
+++ modulo05/src/pages/Repository/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import api from '../../services/api';
+import Container from '../../components/Container';
 
-// import { Container } from './styles';
+import { Loading, Owner } from './styles';
 
 export default class Repository extends Component {
-  static PropTypes = {
+  // eslint-disable-next-line react/static-property-placement
+  static propTypes = {
     match: PropTypes.shape({
       params: PropTypes.shape({
         repository: PropTypes.string,
@@ -42,6 +45,19 @@ export default class Repository extends Component {
 
   render() {
     const { repository, issues, loading } = this.state;
-    return <h1>Respository</h1>;
+    if (loading) {
+      return <Loading>Carregando</Loading>;
+    }
+    return (
+      <Container>
+        <Owner>
+          <Link to="/">Voltar aos repositórios</Link>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <h1>{repository.name}</h1>
+          <p>{repository.description}</p>
+        </Owner>
+        Respository
+      </Container>
+    );
   }
 }
