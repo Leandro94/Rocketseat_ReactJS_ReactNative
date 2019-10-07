779931094a96436995a9a2248a44a0c5234edc4e
LeandroR94 <reisleandro868@gmail.com>
2019-10-07 18:06:25 -0300

    ESlint, Prettier & EditorConfig

---
+++ Primeiro projeto com React Native/modulo06/App.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}> welcome to React Native!</Text>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
