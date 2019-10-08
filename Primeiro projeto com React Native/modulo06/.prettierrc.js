190dbca0d0e0ca808b45df63526d65349724cdea
LeandroR94 <reisleandro868@gmail.com>
2019-10-08 15:23:51 -0300

    Criando novo projeto

--- Primeiro projeto com React Native/modulo06/.prettierrc.js
+++ Primeiro projeto com React Native/modulo06/.prettierrc.js
@@ -2,5 +2,5 @@ module.exports = {
   bracketSpacing: false,
   jsxBracketSameLine: true,
   singleQuote: true,
-  trailingComma: 'es5',
+  trailingComma: 'all',
 };
--- Primeiro projeto com React Native/modulo06/App.js
+++ Primeiro projeto com React Native/modulo06/App.js
@@ -29,3 +29,4 @@ const styles = StyleSheet.create({
     margin: 10,
   },
 });
+
