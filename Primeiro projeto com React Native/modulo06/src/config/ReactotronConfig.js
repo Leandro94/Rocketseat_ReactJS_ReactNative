e94f3706cee17786809f469ede656799de326e07
LeandroR94 <reisleandro868@gmail.com>
2019-10-15 14:30:03 -0300

    Acessando API do Github

--- Primeiro projeto com React Native/modulo06/src/config/ReactotronConfig.js
+++ Primeiro projeto com React Native/modulo06/src/config/ReactotronConfig.js
@@ -1,7 +1,7 @@
 import Reactotron from 'reactotron-react-native';
 
 if (__DEV__) {
-  const tron = Reactotron.configure({ host: '192.168.99.1' })
+  const tron = Reactotron.configure({ host: '10.5.10.137' })
     .useReactNative()
     .connect();
   console.tron = tron;
